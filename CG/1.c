a3c16be535a9051a21474585b5186385cffaaa1a
CherevkoAndrew <cherevkoandrew@gmail.com>
2016-10-12 10:37:41 +0300

    CG lasbs added

---
+++ CG/1.c
@@ -0,0 +1,123 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/* підключаємо бібліотеку GLUT */
+
+#include <GL/glut.h>
+
+const int CubeSize = 200;
+
+int size = 100;
+int xStart = size;
+int yStart = size;
+
+float sqr = 1/1.414;
+
+GLint Width = size*9, Height = size*10;
+
+float triangles[][2] = {{3,2},{3,3},{4,3},
+                  {3,2},{4,3},{5,2},
+                  {5,2},{3,4},{5,4},
+                  {5,4},{3,4},{3,6},
+                  {3,4.-sqr},{3-sqr,4.},{3,4.+sqr}
+                };
+float squares[][2] = {{2,6},{2,7},{3,7},{3,6},
+                      {3,7},{3+sqr,7+sqr},{3+3*sqr,7+sqr},{3+2*sqr,7}
+                     };
+
+int tlen = 5*3;
+int slen = 2*4;
+float transp = 0.05;
+
+/* ця функція керує всім виведенням на екран */
+
+void Display(void){
+  int left = -1, right = 100, top = -1, bottom = 100;
+  glClearColor(0, 0, 0, 1);
+  glClear(GL_COLOR_BUFFER_BIT);
+  glColor3ub(255,0,0);
+
+
+  for(int i = 0; i<tlen; i+=3){
+    glBegin(GL_LINE_LOOP);
+      glVertex2fv(triangles[i]);
+      glVertex2fv(triangles[i+1]);
+      glVertex2fv(triangles[i+2]);
+    glEnd();
+  }
+
+  for(int i = 0; i<slen; i+=4){
+    glBegin(GL_LINE_LOOP);
+      glVertex2fv(squares[i]);
+      glVertex2fv(squares[i+1]);
+      glVertex2fv(squares[i+2]);
+      glVertex2fv(squares[i+3]);
+    glEnd();
+  }
+
+  glFinish();
+}
+
+/* Функція викликається при зміні розмірів вікна */
+
+void Reshape(GLint w, GLint h){
+  Width = w;
+  Height = h;
+  /* установлюємо розміри області відображення */
+  glViewport(0, 0, w, h);
+
+  /* ортографічна проекція */
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glOrtho(0, w/100, 0, h/100, -1.0, 1.0);
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+}
+
+/* Функція обробляє повідомлення від клавіатури */
+
+void Keyboard( unsigned char key, int x, int y ){
+
+  #define ESCAPE '\033'
+  #define PLUS 43
+  #define MINUS 45
+
+  printf("ododod");
+  if( key == ESCAPE )
+    exit(0);
+
+  if(key == PLUS){
+    for (int i = 0; i < tlen; i++) {
+      triangles[i][0] += transp;
+    }
+
+    for (int i = 0; i < slen; i++) {
+      squares[i][0] += transp;
+    }
+  }
+
+  if(key == MINUS){
+    for (int i = 0; i < tlen; i++) {
+      triangles[i][0] -= transp;
+    }
+
+    for (int i = 0; i < slen; i++) {
+      squares[i][0] -= transp;
+    }
+  }
+  glutPostRedisplay();
+}
+
+/* Головний цикл додатка */
+
+main(int argc, char *argv[])
+{
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_RGB);
+  glutInitWindowSize(Width, Height);
+  glutCreateWindow("Red square example");
+  glutDisplayFunc(Display);
+  glutReshapeFunc(Reshape);
+  glutKeyboardFunc(Keyboard);
+  glutMainLoop();
+}
---
+++ CG/cube.c
@@ -0,0 +1,144 @@
+
+/* Copyright (c) Mark J. Kilgard, 1997. */
+
+/* This program is freely distributable without licensing fees
+   and is provided without guarantee or warrantee expressed or
+   implied. This program is -not- in the public domain. */
+
+/* This program was requested by Patrick Earl; hopefully someone else
+   will write the equivalent Direct3D immediate mode program. */
+
+#include <GL/glut.h>
+#include <math.h>
+#include <stdio.h>
+
+
+GLfloat R = 5.;
+double alpha = 0.;
+double beta = 0.;
+
+GLfloat light_diffuse[] = {1.0, 1.0, 1.0, 1.0};  /* Red diffuse light. */
+GLfloat light_position[] = {1.0, 1.0, 1.0, 0.0};  /* Infinite light location. */
+GLfloat n[6][3] = {  /* Normals for the 6 faces of a cube. */
+  {-1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {1.0, 0.0, 0.0},
+  {0.0, -1.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, -1.0} };
+GLint faces[6][4] = {  /* Vertex indices for the 6 faces of a cube. */
+  {0, 1, 2, 3}, {3, 2, 6, 7}, {7, 6, 5, 4},
+  {4, 5, 1, 0}, {5, 6, 2, 1}, {7, 4, 0, 3} };
+GLfloat v[8][3];  /* Will be filled in with X,Y,Z vertexes. */
+
+void
+drawBox(void)
+{
+  int i;
+
+  for (i = 0; i < 6; i++) {
+    glBegin(GL_LINE_LOOP);
+    glNormal3fv(&n[i][0]);
+    glVertex3fv(&v[faces[i][0]][0]);
+    glVertex3fv(&v[faces[i][1]][0]);
+    glVertex3fv(&v[faces[i][2]][0]);
+    glVertex3fv(&v[faces[i][3]][0]);
+    glEnd();
+  }
+
+	glPushMatrix();
+        glTranslated(0.0,0,0.);
+        glutWireSphere(1,16,16);
+    glPopMatrix(); 
+	//glBegin(GL_LINE_LOOP);
+		for(float x = -1.; x < 1.; x+=0.1)
+			for(float y = -1.; y < 1.; y+=0.1 ){
+				glBegin(GL_LINE_LOOP);
+				glVertex3f(x, y, sin(x) + cos(y));
+				glVertex3f(x+0.1, y, sin(x+0.1)+cos(y) );
+				glVertex3f(x+0.1, y+0.1, sin(x+0.1)+cos(y+0.1));
+				glVertex3f(x, y+0.1, sin(x)+cos(y+0.1));
+				glEnd();
+
+		}
+	//glEnd();
+}
+
+void
+display(void)
+{
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  drawBox();
+  glutSwapBuffers();
+}
+
+void
+init(GLint w, GLint h)
+{
+  /* Setup cube vertex data. */
+  v[0][0] = v[1][0] = v[2][0] = v[3][0] = -1;
+  v[4][0] = v[5][0] = v[6][0] = v[7][0] = 1;
+  v[0][1] = v[1][1] = v[4][1] = v[5][1] = -1;
+  v[2][1] = v[3][1] = v[6][1] = v[7][1] = 1;
+  v[0][2] = v[3][2] = v[4][2] = v[7][2] = 1;
+  v[1][2] = v[2][2] = v[5][2] = v[6][2] = -1;
+
+  /* Enable a single OpenGL light. */
+  glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+  glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+  glEnable(GL_LIGHT0);
+  glEnable(GL_LIGHTING);
+
+  /* Use depth buffering for hidden surface elimination. */
+  glEnable(GL_DEPTH_TEST);
+
+  /* Setup the view of the cube. */
+  glMatrixMode(GL_PROJECTION);
+  gluPerspective( /* field of view in degree */ 40.0,
+    /* aspect ratio */ 1.0,
+    /* Z near */ 1.0, /* Z far */ 10.0);
+  glMatrixMode(GL_MODELVIEW);
+  gluLookAt(5., 5.0, 0.0,  /* eye is at (0,0,5) */
+    0.0, 0.0, 0.0,      /* center is at (0,0,0) */
+    0.0, 1.0, 0.);      /* up is in positive Y direction */
+
+  /* Adjust cube position to be asthetic angle. */
+  //glTranslatef(0., 0., -1.0);
+  //glScalef(.5,.5,.5);
+ // glRotatef(60, 1.0, 0.0, 0.0);
+ // glRotatef(-20, 0.0, 0.0, 1.0);
+}
+
+void Keyboard( unsigned char key, int x, int y){
+  
+  #define up 119
+  #define down 115
+  #define left 97
+  #define right 100
+  if(key == left)
+ 	alpha += 0.01;
+  if(key == right)
+	alpha -= 0.01; 
+  if(key == up)
+    beta += 0.01;
+  if(key == down)
+	beta -= 0.01;
+ 
+  glLoadIdentity();
+  glMatrixMode(GL_MODELVIEW);
+  gluLookAt(-R*sin(alpha*M_PI)*cos(beta*M_PI),-R*sin(beta*M_PI),-R*cos(alpha*M_PI)*cos(beta*M_PI),0.,0.,0.,0.,1.,0.);
+
+  
+  printf("%f\n", beta);
+  glutPostRedisplay();
+}
+
+int
+main(int argc, char **argv)
+{
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+  glutCreateWindow("red 3D lighted cube");
+  glutDisplayFunc(display);
+  glutReshapeFunc(init);
+  //init(0,0);
+  glutKeyboardFunc(Keyboard);
+  glutMainLoop();
+  return 0;             /* ANSI C requires main to return int. */
+}
